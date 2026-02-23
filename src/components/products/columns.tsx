2708f00378f73e46a9eba3350c5ec3041428b463
Ratana10 <sanratana18@gmail.com>
2026-02-23 23:30:35 +0700

    20260223: 1. Fetch data using useEffect 2. Render data with data table 3. Formatting cell

--- src/components/products/columns.tsx
+++ src/components/products/columns.tsx
@@ -1,21 +1,23 @@
 "use client";
 
 import type { ColumnDef } from "@tanstack/react-table";
-
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
-// export type Product = {
-//   id: number;
-//   productName: string;
-//   price: number;
-//   quantity: number;
-// };
+import { Badge } from "../ui/badge";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { MoreHorizontal, SquarePen, Trash2 } from "lucide-react";
 
 export interface Product {
   id: number;
-  productName: string;
+  title: string;
   price: number;
-  quantity: number;
+  images: string;
 }
 
 export const columns: ColumnDef<Product>[] = [
@@ -24,15 +26,55 @@ export const columns: ColumnDef<Product>[] = [
     header: "ID",
   },
   {
-    accessorKey: "productName",
     header: "Product name",
+    cell: ({ row }) => <div>{row.original.title}</div>,
   },
   {
     accessorKey: "price",
     header: "Price",
+    cell: ({ row }) => (
+      <div>
+        <Badge className="bg-blue-500">{row.original.price}</Badge>
+      </div>
+    ),
   },
   {
-    accessorKey: "quantity",
-    header: "Quantity",
+    header: "Images",
+    cell: ({ row }) => (
+      <div>
+        <img
+          src={row.original.images[0]}
+          alt=""
+          className="w-[100px] h-[100px]"
+        />
+      </div>
+    ),
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <SquarePen /> Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem className="text-red-500">
+              <Trash2 className="text-red-500" />
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,30 +1,27 @@
 import { columns } from "@/components/products/columns";
 import { DataTable } from "@/components/products/data-table";
 import { Button } from "@/components/ui/button";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Product = () => {
-  const products = [
-    {
-      id: 1,
-      productName: "Laptop",
-      price: 888.99,
-      quantity: 100,
-    },
-    {
-      id: 2,
-      productName: "Phone",
-      price: 888.99,
-      quantity: 100,
-    },
-    {
-      id: 3,
-      productName: "Phone",
-      price: 888.99,
-      quantity: 100,
-    },
-  ];
+  const [products, setProducts] = useState([]);
+
+
+  useEffect(() => {
+
+    const fetchProduct = async () => {
+      const res = await fetch("https://api.escuelajs.co/api/v1/products");
+
+      const data = await res.json();
+
+      console.log("Fetched data", data);
+      setProducts(data);
+      return data;
+    };
+
+    fetchProduct();
+  }, []);
   return (
     <div>
       <DataTable columns={columns} data={products} />
