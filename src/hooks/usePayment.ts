7a3c0cf569d096ebb30669cc904ace1681ac7ed6
Ratana10 <sanratana18@gmail.com>
2026-04-20 23:17:48 +0700

    20260420:
    1. Create transaction api
    2. get tranId from search params
    3. Call check transaction api

--- src/hooks/usePayment.ts
+++ src/hooks/usePayment.ts
@@ -1,4 +1,4 @@
-import { createPayment } from "@/services/payment.service";
+import { checkTransaction, createPayment } from "@/services/payment.service";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 
@@ -18,3 +18,21 @@ export const useCreatePayment = () => {
     },
   });
 };
+
+
+export const useCheckTransaction = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: checkTransaction,
+    onSuccess: () => {
+      toast.success("Transaction checked successfully");
+      queryClient.invalidateQueries({ queryKey: ["payments"] });
+    },
+    onError: (error: Error) => {
+      toast.error("Failed to check transaction");
+      console.log("Failed to check transaction", error);
+    },
+  });
+};
+
